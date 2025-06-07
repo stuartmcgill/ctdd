6c2130b87fdb9923c83ddcd772ab9296ab086998
Stuart McGill <stueymcgill@gmail.com>
2025-06-07 11:59:23 +0100

    Add Tailwind typography

--- resources/views/site/page.blade.php
+++ resources/views/site/page.blade.php
@@ -2,14 +2,11 @@
 <html lang="en">
 <head>
     <title>Demo page</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-<div>
-    Example preview. See <a href="https://twillcms.com/docs/modules/revisions-and-previewing.html">documentation.</a>
-    <br />
-    {{ $item->title }}
-    <br />
-    {{ $item->description }}
+<div class="mx-auto max-w-2xl">
+    {!! $item->renderBlocks() !!}
 </div>
 </body>
 </html>
