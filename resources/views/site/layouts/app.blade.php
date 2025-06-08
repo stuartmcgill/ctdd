d864d771473b01ee5b24b80189a5ecb6b33b535b
Stuart McGill <stueymcgill@gmail.com>
2025-06-08 21:45:30 +0100

    Add meta tags

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Churches Together in Didcot and District')</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
