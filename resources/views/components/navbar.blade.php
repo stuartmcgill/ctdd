77aa0623cb27cd26337e342c4f1e9570d0958100
Stuart McGill <stueymcgill@gmail.com>
2025-06-16 20:05:19 +0100

    Fix menu pos

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="">
     <div class="mx-auto p-8 max-w-screen-xl flex flex-wrap gap-4 items-center justify-between text-white bg-red-600">
         <img src="{{ asset('assets/images/ctdd-logo.png') }}" alt="CTDD Logo" class="order-1 text-white">
-        <a href="/" class="order-2 sm:order-3 flex flex-col gap-2 self-center">
+        <a href="/" class="order-2 md:order-3 flex flex-col gap-2 self-center">
             <span class="text-4xl font-semibold">Churches Together</span>
             <span class="uppercase text-2xl">in Didcot and District</span>
         </a>
