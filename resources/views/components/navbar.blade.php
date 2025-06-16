ab3fd7a5a9bfe715a4128dadce48624ef709c90a
Stuart McGill <stueymcgill@gmail.com>
2025-06-16 20:03:45 +0100

    Add CTDD logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,12 @@
 <nav class="">
-    <div class="mx-auto p-8 max-w-screen-xl flex flex-wrap items-center justify-between text-white bg-red-600">
-        <a href="/" class="flex flex-col gap-2 self-center">
+    <div class="mx-auto p-8 max-w-screen-xl flex flex-wrap gap-4 items-center justify-between text-white bg-red-600">
+        <img src="{{ asset('assets/images/ctdd-logo.png') }}" alt="CTDD Logo" class="order-1 text-white">
+        <a href="/" class="order-2 sm:order-3 flex flex-col gap-2 self-center">
             <span class="text-4xl font-semibold">Churches Together</span>
             <span class="uppercase text-2xl">in Didcot and District</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button"
-                class="text-white bg-red-600 inline-flex items-center p-2 w-10 h-10 justify-center text-sm
+                class="order-2 ml-auto text-white bg-red-600 inline-flex items-center p-2 w-10 h-10 justify-center text-sm
                    rounded-lg md:hidden hover:bg-red-500 focus:outline-none focus:ring-2
                    focus:ring-gray-200"
                 aria-controls="navbar-default" aria-expanded="false">
@@ -13,7 +14,7 @@
                 <path fill-rule="evenodd" d="M3 5h14a1 1 0 100-2H3a1 1 0 100 2zm14 4H3a1 1 0 100 2h14a1 1 0 100-2zm0 6H3a1 1 0 100 2h14a1 1 0 100-2z" clip-rule="evenodd" />
             </svg>
         </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div class="order-3 hidden w-full md:block md:w-auto" id="navbar-default">
             <ul class="bg-red-600 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:rounded-none md:flex-row md:mt-0 md:border-0">
                 @foreach ($links as $link)
                     <li>
