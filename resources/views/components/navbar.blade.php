463edc09c32aa7d5cb7cf3615cfa6fb1dc132133
Stuart McGill <stueymcgill@gmail.com>
2025-06-16 21:46:52 +0100

    Adjust colors on navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,7 +6,7 @@
             <span class="uppercase text-2xl">in Didcot and District</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button"
-                class="order-1 sm:order-2 ml-auto text-white bg-red-600 inline-flex items-center p-2 w-10 h-10 justify-center text-sm
+                class="order-1 sm:order-2 ml-auto inline-flex items-center p-2 w-10 h-10 justify-center text-sm
                    rounded-lg md:hidden hover:bg-red-500 focus:outline-none focus:ring-2
                    focus:ring-gray-200"
                 aria-controls="navbar-default" aria-expanded="false">
@@ -15,11 +15,11 @@
             </svg>
         </button>
         <div class="order-3 hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="bg-red-600 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:rounded-none md:flex-row md:mt-0 md:border-0">
+            <ul class="bg-red-100 md:bg-red-600 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:rounded-none md:flex-row md:mt-0 md:border-0">
                 @foreach ($links as $link)
                     <li>
                         <a href="{{route('frontend.page', [$link->getRelated('page')->first()->slug])}}"
-                        class="block py-2 px-3 text-white rounded md:rounded-none hover:text-red-700 hover:bg-gray-100 md:hover:bg-transparent
+                        class="block py-2 px-3 text-red-600 md:text-white rounded md:rounded-none hover:text-red-700 hover:bg-gray-100 md:hover:bg-transparent
                       md:hover:text-red-100 md:p-3 md:px-2">
                             {{ $link->title }}
                         </a>
