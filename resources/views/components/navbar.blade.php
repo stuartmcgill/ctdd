40771dbe6738ac0893de8480ea405edd0102dc77
Stuart McGill <stueymcgill@gmail.com>
2025-06-28 15:48:14 +0100

    Add link to logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,11 @@
 <nav class="">
   <div class="mx-auto p-8 max-w-screen-xl flex flex-wrap gap-8 items-center justify-between text-white bg-red-600">
-    <img src="{{ asset('assets/images/ctdd-logo.png') }}" alt="CTDD Logo" class="order-0 text-white">
+    <a href="/" class="order-0 text-white">
+      <img
+        src="{{ asset('assets/images/ctdd-logo.png') }}"
+        alt="CTDD Logo"
+      >
+    </a>
     <a href="/" class="order-2 md:order-3 flex flex-col gap-2 self-center w-full sm:w-auto text-center sm:text-left">
       <span class="text-2xl sm:text-4xl font-semibold">Churches Together</span>
       <span class="uppercase text-lg sm:text-2xl">in Didcot and District</span>
