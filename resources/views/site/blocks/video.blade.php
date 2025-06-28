56aba2623bd547d886c4fccc471dfa3ee27b875d
Stuart McGill <stueymcgill@gmail.com>
2025-06-28 16:10:43 +0100

    Add embedded video block

---
+++ resources/views/site/blocks/video.blade.php
@@ -0,0 +1,3 @@
+<div class="video-wrapper">
+  {!! $block->input('embeddedText') !!}
+</div>
---
+++ resources/views/twill/blocks/video.blade.php
@@ -0,0 +1,22 @@
+@twillBlockTitle('Video link')
+@twillBlockIcon('video')
+@twillBlockGroup('app')
+@twillBlockValidationRules([
+'name' => 'required',
+'embeddedText' => 'required',
+])
+
+<x-twill::input
+  name="name"
+  label="Name"
+  :required="true"
+  note="For internal use only"
+/>
+
+<x-twill::input
+  name="embeddedText"
+  label="Copy the embed video HTML here"
+  :required="true"
+  type="text"
+  placeholder='e.g. <iframe width="560" height="315" src="https://www...</iframe>'
+/>
