fff8ddee763eb4c9b70834d99657e7656edd5b0a
Stuart McGill <stueymcgill@gmail.com>
2025-06-29 18:46:17 +0100

    Temporarily protect FE to non-admins

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 use App\Http\Controllers\ArticleDisplayController;
 use App\Http\Controllers\ChurchDisplayController;
 use App\Http\Controllers\PageDisplayController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/churches/{slug}', [ChurchDisplayController::class, 'show'])->name('frontend.church');
-Route::get('/churches/', [ChurchDisplayController::class, 'list'])->name('frontend.church.list');
+// @TODO - remove this middleware
+Route::group([
+    'middleware' => function ($request, $next) {
+        if (! Auth::guard('twill_users')->check()) {
+            abort(403, 'Frontend temporarily restricted to administrators.');
+        }
 
-Route::get('/articles/{slug}', [ArticleDisplayController::class, 'show'])->name('frontend.article');
-Route::get('/articles/', [ArticleDisplayController::class, 'list'])->name('frontend.article.list');
+        return $next($request);
+    },
+], function () {
+    Route::get('/churches/{slug}', [ChurchDisplayController::class, 'show'])->name('frontend.church');
+    Route::get('/churches/', [ChurchDisplayController::class, 'list'])->name('frontend.church.list');
 
-Route::get('{slug}', [PageDisplayController::class, 'show'])->name('frontend.page');
-Route::get('/', [PageDisplayController::class, 'home'])->name('frontend.home');
+    Route::get('/articles/{slug}', [ArticleDisplayController::class, 'show'])->name('frontend.article');
+    Route::get('/articles/', [ArticleDisplayController::class, 'list'])->name('frontend.article.list');
+
+    Route::get('{slug}', [PageDisplayController::class, 'show'])->name('frontend.page');
+    Route::get('/', [PageDisplayController::class, 'home'])->name('frontend.home');
+});
