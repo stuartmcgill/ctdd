0eb45ad34fcec2e4dc655fb3cb59c163a9ef57a5
Stuart McGill <stueymcgill@gmail.com>
2025-06-29 16:35:43 +0100

    Improve card styling

--- resources/views/components/article-card.blade.php
+++ resources/views/components/article-card.blade.php
@@ -1,10 +1,10 @@
 <a href="{{ $article->url() }}"
    class="app-card">
   <x-twill-image :item="$article"/>
-  <div class="p-4">
+  <div class="pt-0 p-4">
     <h2 class="mb-2 app-card-heading">{{ $article->title }}</h2>
     <p class="mb-3 font-normal text-gray-700">{{ $article->description }}</p>
-    <div class="flex items-center justify-between">
+    <div class="flex items-center justify-between mt-auto">
       <x-more-button/>
       <div class="uppercase text-gray-500">{{ $article->created_at->format('d M Y') }}</div>
     </div>
--- resources/views/components/church-card.blade.php
+++ resources/views/components/church-card.blade.php
@@ -1,9 +1,11 @@
 <a href="{{ $church->url() }}"
-   class="app-card">
+   class="app-card flex flex-col h-full">
   <x-twill-image :item="$church"/>
-  <div class="p-4">
+  <div class="pt-0 p-4 h-full flex flex-col">
     <h2 class="mb-2 app-card-heading">{{ $church->title }}</h2>
     <p class="mb-3 font-normal text-gray-700">{{ $church->location }}</p>
-    <x-more-button/>
+    <div class="mt-auto">
+      <x-more-button/>
+    </div>
   </div>
 </a>
