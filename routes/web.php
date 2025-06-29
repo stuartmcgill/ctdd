3ec8995efcb381a64080651c9afcc79ee2a257e7
Stuart McGill <stueymcgill@gmail.com>
2025-06-29 18:38:53 +0100

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ use App\Http\Controllers\ChurchDisplayController;
 use App\Http\Controllers\PageDisplayController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/churches/{slug}', [ChurchDisplayController::class, 'show'])->name('frontend.church');
 Route::get('/churches/', [ChurchDisplayController::class, 'list'])->name('frontend.church.list');
 
