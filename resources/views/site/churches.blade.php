2b3e2654616dff671886ac2431b595870ac76473
Stuart McGill <stueymcgill@gmail.com>
2025-08-25 10:07:16 +0100

    Move group to component

--- resources/views/site/churches.blade.php
+++ resources/views/site/churches.blade.php
@@ -19,13 +19,7 @@
   </div>
   @foreach($groups as $group)
     <div class="app-section">
-      <h1>{{ $group->title }}</h1>
-      <div class="mb-8">{!! $group->description !!}</div>
-      <div class="lg:-mx-32 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        @foreach($group->churches as $church)
-          <x-church-card :church="$church"/>
-        @endforeach
-      </div>
+      <x-group :group="$group"/>
     </div>
   @endforeach
   <div id="church-near-you" class="app-section flex flex-col">
--- resources/views/site/group.blade.php
+++ resources/views/site/group.blade.php
@@ -1,15 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Demo page</title>
-</head>
-<body>
-<div>
-    Example preview. See <a href="https://twillcms.com/docs/modules/revisions-and-previewing.html">documentation.</a>
-    <br />
-    {{ $item->title }}
-    <br />
-    {{ $item->description }}
-</div>
-</body>
-</html>
+@extends('site.layouts.app')
+
+@section('title', $item->title)
+
+@section('content')
+  <x-group :group="$item"/>
+@endsection
