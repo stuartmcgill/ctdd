fd4899ac57cd353e9f364cbb663055e468a850bd
Stuart McGill <stueymcgill@gmail.com>
2025-09-08 22:22:28 +0100

    Better formatting

--- resources/views/site/blocks/recent-news.blade.php
+++ resources/views/site/blocks/recent-news.blade.php
@@ -12,11 +12,14 @@
       <div class="flex flex-col">
         <a href="{{ $primary->url() }}" class="grid grid-cols-1 sm:grid-cols-[67%_33%] gap-4 app-card">
           <x-twill-image :item="$primary" class="rounded-t-lg rounded-b-none sm:rounded-l-lg sm:rounded-r-none"/>
-          <div class="p-4 text-xl font-semibold">{{ $primary->description }}</div>
+          <div class="p-4 flex flex-col gap-3">
+            <div class="mt-2 sm:mt-6 text-xl font-semibold uppercase">{{ $primary->title }}</div>
+            <div>{{ $primary->description }}</div>
+          </div>
         </a>
       </div>
       @if($others->isNotEmpty())
-        <div class="flex items-center justify-between gap-4">
+        <div class="mt-6 flex items-center justify-between gap-4">
           <h2>Latest news</h2>
           <a href="/articles" class="mt-8 mb-4 text-md !no-underline">See all →</a>
         </div>
@@ -25,7 +28,10 @@
           @foreach($others as $other)
             <a href="{{ $other->url() }}" class="flex flex-col app-card">
               <x-twill-image :item="$other" class="rounded-b-none rounded-t-lg"/>
-              <div class="p-4 text-lg font-semibold">{{ $other->description }}</div>
+              <div class="p-4 flex flex-col gap-3">
+                <div class="mt-4 text-lg font-semibold uppercase">{{ $other->title }}</div>
+                <div>{{ $other->description }}</div>
+              </div>
             </a>
           @endforeach
         </div>
