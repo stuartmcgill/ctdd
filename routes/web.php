8a537f404f868be6def029797a4812e82487c13b
Stuart McGill <stueymcgill@gmail.com>
2026-03-17 19:49:47 +0000

    Remove guard middleware

--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,13 @@ declare(strict_types=1);
 use App\Http\Controllers\ArticleDisplayController;
 use App\Http\Controllers\ChurchDisplayController;
 use App\Http\Controllers\PageDisplayController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// @TODO - remove this middleware
-Route::group([
-    'middleware' => function ($request, $next) {
-        if (! app()->environment('local') && ! Auth::guard('twill_users')->check()) {
-            abort(403, 'Frontend temporarily restricted to administrators.');
-        }
+Route::get('/churches/{slug}', [ChurchDisplayController::class, 'show'])->name('frontend.church');
+Route::get('/churches/', [ChurchDisplayController::class, 'list'])->name('frontend.church.list');
 
-        return $next($request);
-    },
-], function () {
-    Route::get('/churches/{slug}', [ChurchDisplayController::class, 'show'])->name('frontend.church');
-    Route::get('/churches/', [ChurchDisplayController::class, 'list'])->name('frontend.church.list');
+Route::get('/articles/{slug}', [ArticleDisplayController::class, 'show'])->name('frontend.article');
+Route::get('/articles/', [ArticleDisplayController::class, 'list'])->name('frontend.article.list');
 
-    Route::get('/articles/{slug}', [ArticleDisplayController::class, 'show'])->name('frontend.article');
-    Route::get('/articles/', [ArticleDisplayController::class, 'list'])->name('frontend.article.list');
-
-    Route::get('{slug}', [PageDisplayController::class, 'show'])->name('frontend.page');
-    Route::get('/', [PageDisplayController::class, 'home'])->name('frontend.home');
-});
+Route::get('{slug}', [PageDisplayController::class, 'show'])->name('frontend.page');
+Route::get('/', [PageDisplayController::class, 'home'])->name('frontend.home');
