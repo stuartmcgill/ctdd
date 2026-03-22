1abfce759196edf115c3f531770e7a6bcf1446d9
Stuart McGill <stueymcgill@gmail.com>
2026-03-22 14:08:50 +0000

    Spacing improvements, remove article date

--- resources/views/components/article-card.blade.php
+++ resources/views/components/article-card.blade.php
@@ -1,12 +1,11 @@
 <a href="{{ $article->url() }}"
-   class="sm:max-w-sm app-card">
+   class="sm:max-w-sm app-card flex flex-col">
   <x-twill-image :item="$article" class="rounded-b-none rounded-t-lg"/>
-  <div class="pt-0 p-4">
+  <div class="pt-0 p-4 flex flex-col h-full">
     <h2 class="mb-2 app-card-heading">{{ $article->title }}</h2>
     <p class="mb-3 font-normal text-gray-700">{{ $article->description }}</p>
-    <div class="flex items-center justify-between mt-auto">
+    <div class="mt-auto">
       <x-more-button/>
-      <div class="uppercase text-gray-500">{{ $article->created_at->format('d M Y') }}</div>
     </div>
   </div>
 </a>
